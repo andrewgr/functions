505bc7e736c41523746af65ad88618f4399c8288
Andrei Gridnev <andrew.gridnev@gmail.com>
2018-04-08 17:13:26 +1000

    Add DSL module

--- lib/functions.rb
+++ lib/functions.rb
@@ -1,18 +1,18 @@
-require 'functions/version'
 require 'functions/dsl'
+require 'functions/version'
 
-Functions.number(:zero,  0)
-Functions.number(:one,   1)
-Functions.number(:two,   2)
-Functions.number(:three, 3)
-Functions.number(:four,  4)
-Functions.number(:five,  5)
-Functions.number(:six,   6)
-Functions.number(:seven, 7)
-Functions.number(:eight, 8)
-Functions.number(:nine,  9)
+Functions::DSL.number(:zero,  0)
+Functions::DSL.number(:one,   1)
+Functions::DSL.number(:two,   2)
+Functions::DSL.number(:three, 3)
+Functions::DSL.number(:four,  4)
+Functions::DSL.number(:five,  5)
+Functions::DSL.number(:six,   6)
+Functions::DSL.number(:seven, 7)
+Functions::DSL.number(:eight, 8)
+Functions::DSL.number(:nine,  9)
 
-Functions.operation(:plus)       { |a, b| a + b }
-Functions.operation(:minus)      { |a, b| a - b }
-Functions.operation(:times)      { |a, b| a * b }
-Functions.operation(:divided_by) { |a, b| a / b }
+Functions::DSL.operation(:plus)       { |a, b| a + b }
+Functions::DSL.operation(:minus)      { |a, b| a - b }
+Functions::DSL.operation(:times)      { |a, b| a * b }
+Functions::DSL.operation(:divided_by) { |a, b| a / b }
--- lib/functions/dsl.rb
+++ lib/functions/dsl.rb
@@ -1,27 +1,29 @@
 module Functions
-  Identity = -> (x) { x }
+  module DSL
+    Identity = -> (x) { x }
 
-  class << self
-    attr_accessor :global_scope
+    class << self
+      attr_accessor :global_scope
 
-    def number(name, value)
-      global_scope.instance_exec do
-        define_method name do |operation = Identity|
-          operation.call(value)
+      def number(name, value)
+        global_scope.instance_exec do
+          define_method name do |operation = Identity|
+            operation.call(value)
+          end
+          private name
         end
-        private name
       end
-    end
 
-    def operation(name, &block)
-      global_scope.instance_exec do
-        define_method name do |b|
-          -> a { block.call(a, b) }
+      def operation(name, &block)
+        global_scope.instance_exec do
+          define_method name do |b|
+            -> a { block.call(a, b) }
+          end
+          private name
         end
-        private name
       end
     end
   end
 end
 
-Functions.global_scope = self
+Functions::DSL.global_scope = self
