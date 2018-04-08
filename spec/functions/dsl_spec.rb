842f79bc26e6e6ef9cb2cbdbc920c0682cb1295f
Andrei Gridnev <andrew.gridnev@gmail.com>
2018-04-08 18:09:48 +1000

    Improve testing for the DSL

--- spec/functions/dsl_spec.rb
+++ spec/functions/dsl_spec.rb
@@ -15,10 +15,12 @@ RSpec.describe Functions::DSL do
   describe ".number" do
     it "creates a method in the global scope" do
       expect {
-        described_class.number(:foo_bar_baz, "foo")
+        described_class.number(:foo, "foo")
       }.to change {
-        Object.__send__(:foo_bar_baz) rescue nil
-      }.from(nil).to("foo")
+        Object.__send__(:foo) rescue nil
+      }.from(nil)
+
+      Object.remove_method(:foo)
     end
 
     describe "generated method" do
@@ -36,6 +38,28 @@ RSpec.describe Functions::DSL do
         it "calls the proc and return its result" do
           expect(foo_bar(proc)).to eq(proc.call(10))
         end
+      end
+    end
+  end
+
+  describe ".operation" do
+    it "creates a method in the global scope" do
+      expect {
+        described_class.operation(:foo) { |a, b| a + b }
+      }.to change {
+        Object.__send__(:foo, 1) rescue nil
+      }.from(nil)
+
+      Object.remove_method(:foo)
+    end
+
+    describe "generated method" do
+      before { described_class.operation(:foo_bar) { |a, b| a / b } }
+
+      after { Object.remove_method(:foo_bar) }
+
+      it "right-curryes the block" do
+        expect(foo_bar(2).call(6)).to eq(3)
       end
     end
   end
