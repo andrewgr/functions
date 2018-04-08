28f914e1fedf58319b4c1e14c836e2b8f5a1fc52
Andrei Gridnev <andrew.gridnev@gmail.com>
2018-04-08 11:35:29 +1000

    Use arithmetic expressions in specs, test division by zero

--- spec/functions_spec.rb
+++ spec/functions_spec.rb
@@ -40,28 +40,32 @@ end
 
 RSpec.describe "#times" do
   specify do
-    expect(six(times(five))).to eq(30)
+    expect(six(times(five))).to eq(6 * 5)
   end
 
-  specify "multiplication is commutative" do
+  it "is commutative" do
     expect(six(times(five))).to eq(five(times(six)))
   end
 end
 
 RSpec.describe "#divided_by" do
   specify do
-    expect(six(divided_by(5))).to eq(1)
+    expect(six(divided_by(5))).to eq(6 / 5)
+  end
+
+  specify do
+    expect { one(divided_by(zero)) }.to raise_error(ZeroDivisionError)
   end
 end
 
 RSpec.describe "#plus" do
   specify do
-    expect(one(plus(nine))).to eq(10)
+    expect(one(plus(nine))).to eq(1 + 9)
   end
 end
 
 RSpec.describe "#minus" do
   specify do
-    expect(six(minus(three))).to eq(3)
+    expect(six(minus(three))).to eq(6 - 3)
   end
 end
